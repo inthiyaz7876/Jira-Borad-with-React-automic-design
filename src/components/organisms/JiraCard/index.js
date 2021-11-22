caaf6d0bab59e261759d81044bf7db978d7e16f9
inthiyaz7876 <inthu7876@gmail.com>
2021-11-22 16:14:57 +0530

    Tooltip issue fixes

--- src/components/organisms/JiraCard/index.js
+++ src/components/organisms/JiraCard/index.js
@@ -3,13 +3,12 @@ import Card from '../../atoms/Card';
 import TickButton from '../../molecules/TickButton';
 
 const JiraCard = ({ content }) => {
-  console.log('con', content);
   const contentItem = (item) => {
-    console.log('iii', item);
     return (
       <div>
         <div class="card_title">{item.title}</div>
         <div class="card_description">{item.description}</div>
+        <div class="priority">{item.priority}</div>
         <span class="tooltip">
           <img
             src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
@@ -17,6 +16,7 @@ const JiraCard = ({ content }) => {
           ></img>
           <span class="tooltiptext">{item.assignee}</span>
         </span>
+
         <TickButton />
       </div>
     );
