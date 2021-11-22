99e7aae141f906307c7b5d50bd4171e09555d785
inthiyaz7876 <inthu7876@gmail.com>
2021-11-22 16:01:54 +0530

    2 files modified

--- src/components/organisms/JiraCard/index.js
+++ src/components/organisms/JiraCard/index.js
@@ -10,10 +10,13 @@ const JiraCard = ({ content }) => {
       <div>
         <div class="card_title">{item.title}</div>
         <div class="card_description">{item.description}</div>
-        <img
-          src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-          class="user"
-        ></img>
+        <span class="tooltip">
+          <img
+            src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+            class="user"
+          ></img>
+          <span class="tooltiptext">{item.assignee}</span>
+        </span>
         <TickButton />
       </div>
     );
